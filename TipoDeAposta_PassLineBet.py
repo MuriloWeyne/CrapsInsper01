6e33fecd63df9c4dd56472463b8d390b2d6b30a5
Bruno-Boldrim-Saboya <62663074+Bruno-Boldrim-Saboya@users.noreply.github.com>
2020-04-02 14:48:07 -0300

    Add files via upload
    Este arquivo possui o código do tipo de aposta PassLineBet

---
+++ TipoDeAposta_PassLineBet.py
@@ -0,0 +1,12 @@
+import random
+if lista_de_apostasComeOut[0] > 0:
+    soma_plb = random.randint(1,6) + random.randint(1,6)
+    if soma_plb == 7 or soma_plb == 11:
+        saldo += valor_aposta_plb
+        print("Você ganhou sua aposta do tipo Pass Line Bet! Seu saldo agora é de {0} fichas." .format(saldo))
+    elif soma_plb == 2 or soma_plb == 3 or soma_plb == 12:
+        saldo -= valor_aposta_plb
+        print("Você perdeu sua aposta do tipo Pass Line Bet! Seu saldo agora é de {0} fichas." .format(saldo))
+    else:
+        fase = "Point"
+        print("Agora você está na fase Point. Não poderão ser feitas mais apostas do tipo Pass Line Bet")
